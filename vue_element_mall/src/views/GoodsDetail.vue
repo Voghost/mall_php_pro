71832ea71381f13cde544e783148470bf557848b
996647034@qq.com <7890nmzxcvbq>
2021-09-14 20:04:39 +0800

    commit-GoodsDetail

--- vue_element_mall/src/views/GoodsDetail.vue
+++ vue_element_mall/src/views/GoodsDetail.vue
@@ -1,12 +1,37 @@
 <template>
 <div>
-  商品详情
+  <el-container>
+
+    <MallHeader/>
+
+    <!--功能区-->
+    <el-header class="" style="height: 180px; width: 100%">
+      <SearchHeader style="box-shadow: rgba(0,0,0,0.3) 0 0 7px"/>
+    </el-header>
+
+    <el-main>
+      <GoodsDetails/>
+    </el-main>
+
+    <MallFooter/>
+
+  </el-container>
 </div>
 </template>
 
 <script>
+import GoodsDetails from "@/components/GoodsDetails";
+import MallFooter from "@/components/MallFooter";
+import MallHeader from "@/components/MallHeader";
+import SearchHeader from "@/components/SearchHeader";
 export default {
-  name: "GoodsDetail"
+  name: "GoodsDetail",
+  components : {
+    GoodsDetails,
+    MallFooter,
+    MallHeader,
+    SearchHeader
+  }
 }
 </script>
 
