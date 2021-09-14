2229d7328f16bc7081dc6b4d64a957171f22729d
tasty_mou <961285150@qq.com>
2021-09-14 16:12:59 +0800

    列表订单（全部订单 待付款 待收货 待评价）操作按钮未完成

---
+++ vue_element_mall/src/components/ItemOrder.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-tabs :tab-position="tabPosition" style="height: auto;">
+    <el-tab-pane label="全部订单">
+      <ItemList></ItemList><!--订单状态-->
+    </el-tab-pane>
+    <el-tab-pane label="待付款">
+      <NopayList></NopayList>
+    </el-tab-pane>
+    <el-tab-pane label="待收货">
+      <PaidList></PaidList>
+    </el-tab-pane>
+    <el-tab-pane label="待评价">
+      <ReceivedList></ReceivedList>
+    </el-tab-pane>
+  </el-tabs>
+
+</template>
+
+<script>
+import ItemList from "./ItemList";
+import NopayList from "@/components/NopayList";
+import PaidList from "@/components/PaidList";
+import ReceivedList from "@/components/ReceivedList";
+export default {
+  name: "ItemOrder",
+  components: {
+    ReceivedList,
+    ItemList,
+    NopayList,
+    PaidList,
+  },
+  data() {
+    return {
+      tabPosition: 'left'
+    };
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
