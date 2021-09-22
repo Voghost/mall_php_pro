ebb47e473a8fa06bc1b438fcb66b1ab9371b2e59
voghost <voghost2@gmail.com>
2021-09-22 09:06:36 +0800

    头部栏动态变化

--- vue_element_mall/src/components/MallHeader.vue
+++ vue_element_mall/src/components/MallHeader.vue
@@ -2,11 +2,15 @@
   <!--标题头部-->
   <el-header class="main" height="30px">
 
-    <div class="left">
+    <div class="left" v-if="Object.keys(userInfo).length === 0">
       <el-link href="/Login_Register" class="link"><i class="el-icon-user">登录</i></el-link>
       <el-divider direction="vertical"></el-divider>
       <el-link href="/Login_Register"><i class="el-icon-top-right"></i>注册</el-link>
     </div>
+    <div class="left" v-if="Object.keys(userInfo).length > 0">
+      <el-link href=""> 欢迎您 {{userInfo.user_name}}</el-link>
+    </div>
+
     <div class="right">
       <el-link href="#" class="link"><i class="el-icon-s-custom">我的资料</i></el-link>
       <el-divider direction="vertical"></el-divider>
@@ -21,6 +25,17 @@
 <script>
 export default {
   name: "MallHeader",
+  data() {
+    return {
+      "userInfo": {},
+    }
+  },
+  created() {
+    this.userInfo = this.$store.state.userInfo;
+    console.log("info", this.userInfo);
+  },
+  methods: {}
+
 }
 </script>
 
